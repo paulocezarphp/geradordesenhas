cb87059f966c32e21dfdf2ac51ced829fe65615b
Cezar Leigos <cezarleigos28@outlook.com>
2021-03-10 21:11:37 -0300

    v0.3 - Adicionando Alfabeto
    Dessa vez nós começamos a adicionar o nosso alfabeto de palavras dentro do nosso arquivo javascript para que possamos gerar nossas senhas, cada alfabeto será responsável por gerar um determinado tipo de caractere.

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,34 @@
 function GerarSenha(tamanho, checkbox_minusculo, checkbox_maiusculo, checkbox_especial, checkbox_numero){
+    
+    var contador_valida = 0;
+    var alfabeto;
+
+    if(tamanho > 0){
+
+    }
+
+    if(checkbox_minusculo == true){
+    	var alfabeto = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','k'];
+        contador_valida++;   
+    }
+
+    if(checkbox_maiusculo == true){
+    	var alfabeto = ['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','X','Z','W','Y','K'];
+        contador_valida++;   
+    }
+
+    if(checkbox_especial == true){
+    	var alfabeto = ['!','@','#','$','%','&','*','(',')','+','-','{','}','[',']','|','=',';','/'];
+        contador_valida++;   
+    }
+
+    if(checkbox_numero == true){
+    	var alfabeto = ['0','1','2','3','4','5','6','7','8','9'];
+        contador_valida++;
+    }
+
+    if(contador_valida == 0){
+        tamanho = 10;
+    }
 
 }
