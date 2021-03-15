da5e8a39ac52e518de1f42d0863b676262a81fdb
Paulo <cezarleigos28@outlook.com>
2021-03-15 11:47:10 -0300

    v0.8 - Terminando de gerar senhas
    Dessa vez nós terminamos de implementar a função que executa a criação da senha.

--- app/js/script.js
+++ app/js/script.js
@@ -30,58 +30,77 @@ document.getElementById("check4").addEventListener("click", function(){
     }
 });
 
-document.getElementById("check4").addEventListener("click", function(){
-    if(document.getElementById("checkbox4").checked == false){
-        document.getElementById("checkbox4").checked = true;
-    }else{
-        document.getElementById("checkbox4").checked = false;
-    }
-});
 
-function GerarSenha(tamanho, checkbox_minusculo, checkbox_maiusculo, checkbox_especial, checkbox_numero){
+
+function GerarSenha(){
     
-    var recebe_tamanho = tamanho;
     var contador_valida = 0;
     var alfabeto;
+    var somaalfabeto = [''];
     var resultado_gerado = "";
+    
+    var recebe_tamanho = document.getElementById("item-input").value;
+    var checkbox_minusculo = document.getElementById("checkbox1").checked;
+    var checkbox_maiusculo = document.getElementById("checkbox2").checked;
+    var checkbox_especial = document.getElementById("checkbox3").checked;
+    var checkbox_numero = document.getElementById("checkbox4").checked;
 
-    if(recebe_tamanho > 0 && recebe_tamanho != null){
+    if(recebe_tamanho >= 0 && recebe_tamanho != null){
 
     }else{
         recebe_tamanho = 10;
     }
 
-    if(!checkbox_minusculo == true){
-        var alfabeto = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','k'];
+    if(!checkbox_minusculo == true && checkbox_maiusculo == false && checkbox_especial == false && checkbox_numero == false){
+        alfabeto = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','k'];
+        somaalfabeto = somaalfabeto.concat(alfabeto);
         contador_valida++;   
     }
 
     if(checkbox_minusculo == true){
-    	var alfabeto = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','k'];
+    	alfabeto = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','k'];
+        somaalfabeto = somaalfabeto.concat(alfabeto);
         contador_valida++;   
     }
 
     if(checkbox_maiusculo == true){
-    	var alfabeto = ['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','X','Z','W','Y','K'];
+    	alfabeto = ['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','X','Z','W','Y','K'];
+        somaalfabeto = somaalfabeto.concat(alfabeto);
         contador_valida++;   
     }
 
     if(checkbox_especial == true){
-    	var alfabeto = ['!','@','#','$','%','&','*','(',')','+','-','{','}','[',']','|','=',';','/'];
+    	alfabeto = ['!','@','#','$','%','&','*','(',')','+','-','{','}','[',']','|','=',';','/'];
+        somaalfabeto = somaalfabeto.concat(alfabeto);
         contador_valida++;   
     }
 
     if(checkbox_numero == true){
-    	var alfabeto = ['0','1','2','3','4','5','6','7','8','9'];
+    	alfabeto = ['0','1','2','3','4','5','6','7','8','9'];
+        somaalfabeto = somaalfabeto.concat(alfabeto);
         contador_valida++;
     }
 
-    if(contador_valida == 0){
-        tamanho = 10;
-    }
+    var suporte;
+    for(var i = 1; i <= parseInt(recebe_tamanho); i++){
+       
+        suporte = somaalfabeto[Math.floor(Math.random() * somaalfabeto.length)];
+        if(suporte != ""){
+            resultado_gerado = resultado_gerado + suporte;
+        }else{
 
-    for(var i = 0; i <= recebe_tamanho; i++){
-       resultado_gerado = resultado_gerado + "" + alfabeto[Math.floor(Math.random() * alfabeto.length)];
+            suporte = somaalfabeto[Math.floor(Math.random() * somaalfabeto.length)];           
+            if(suporte != ""){
+               resultado_gerado = resultado_gerado + suporte;
+            }else{
+               suporte = somaalfabeto[Math.floor(Math.random() * somaalfabeto.length)];
+               resultado_gerado = resultado_gerado + suporte;
+            }
+
+        }
+
+       
+
     }
     
     document.getElementById("item-resultado").innerHTML = "" + resultado_gerado;
@@ -91,7 +110,6 @@ function GerarSenha(tamanho, checkbox_minusculo, checkbox_maiusculo, checkbox_es
 document.getElementById("botaogerar").addEventListener("click", function(){
     
     GerarSenha();
-
 
 });
 
