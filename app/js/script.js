19a78a464533e1e3cc1081881a68ebaad38f2b25
Cezar Leigos <cezarleigos28@outlook.com>
2021-03-14 22:30:45 -0300

    v0.7 - Começando a implementar gerador
    Dessa vez nós começamos a implementar o gerador de senhas, agora falta apenas que implementemos o resto das funções.

--- app/js/script.js
+++ app/js/script.js
@@ -1,39 +1,3 @@
-function GerarSenha(tamanho, checkbox_minusculo, checkbox_maiusculo, checkbox_especial, checkbox_numero){
-    
-    var contador_valida = 0;
-    var alfabeto;
-
-    if(tamanho > 0){
-
-    }
-
-    if(checkbox_minusculo == true){
-    	var alfabeto = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','k'];
-        contador_valida++;   
-    }
-
-    if(checkbox_maiusculo == true){
-    	var alfabeto = ['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','X','Z','W','Y','K'];
-        contador_valida++;   
-    }
-
-    if(checkbox_especial == true){
-    	var alfabeto = ['!','@','#','$','%','&','*','(',')','+','-','{','}','[',']','|','=',';','/'];
-        contador_valida++;   
-    }
-
-    if(checkbox_numero == true){
-    	var alfabeto = ['0','1','2','3','4','5','6','7','8','9'];
-        contador_valida++;
-    }
-
-    if(contador_valida == 0){
-        tamanho = 10;
-    }
-
-}
-
-
 document.getElementById("check1").addEventListener("click", function(){
     if(document.getElementById("checkbox1").checked == false){
         document.getElementById("checkbox1").checked = true;
@@ -73,5 +37,64 @@ document.getElementById("check4").addEventListener("click", function(){
         document.getElementById("checkbox4").checked = false;
     }
 });
+
+function GerarSenha(tamanho, checkbox_minusculo, checkbox_maiusculo, checkbox_especial, checkbox_numero){
+    
+    var recebe_tamanho = tamanho;
+    var contador_valida = 0;
+    var alfabeto;
+    var resultado_gerado = "";
+
+    if(recebe_tamanho > 0 && recebe_tamanho != null){
+
+    }else{
+        recebe_tamanho = 10;
+    }
+
+    if(!checkbox_minusculo == true){
+        var alfabeto = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','k'];
+        contador_valida++;   
+    }
+
+    if(checkbox_minusculo == true){
+    	var alfabeto = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','x','z','w','y','k'];
+        contador_valida++;   
+    }
+
+    if(checkbox_maiusculo == true){
+    	var alfabeto = ['A','B','C','D','E','F','G','H','I','J','L','M','N','O','P','Q','R','S','T','U','V','X','Z','W','Y','K'];
+        contador_valida++;   
+    }
+
+    if(checkbox_especial == true){
+    	var alfabeto = ['!','@','#','$','%','&','*','(',')','+','-','{','}','[',']','|','=',';','/'];
+        contador_valida++;   
+    }
+
+    if(checkbox_numero == true){
+    	var alfabeto = ['0','1','2','3','4','5','6','7','8','9'];
+        contador_valida++;
+    }
+
+    if(contador_valida == 0){
+        tamanho = 10;
+    }
+
+    for(var i = 0; i <= recebe_tamanho; i++){
+       resultado_gerado = resultado_gerado + "" + alfabeto[Math.floor(Math.random() * alfabeto.length)];
+    }
+    
+    document.getElementById("item-resultado").innerHTML = "" + resultado_gerado;
+
+}
+
+document.getElementById("botaogerar").addEventListener("click", function(){
+    
+    GerarSenha();
+
+
+});
+
+
 
 
